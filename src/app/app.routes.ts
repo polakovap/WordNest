ae35c294d2f97fa449cc7237f8eeaeba9d5a5906
polakovap <123546884+polakovap@users.noreply.github.com>
2024-05-14 14:59:21 +0100

    deleted user component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { Routes } from '@angular/router';
 import { FlashcardComponent } from './components/flashcard/flashcard.component';
 import { SynonymComponent } from './components/synonym/synonym.component';
 import { RhymeComponent } from './components/rhyme/rhyme.component';
-import { UserComponent } from './components/user/user.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 //declare routes
@@ -11,7 +10,6 @@ export const routes: Routes = [
     {path:'', component:FlashcardComponent},
     {path: 'synonyms', component:SynonymComponent},
     {path:'rhymes', component:RhymeComponent},
-    {path:'user', component:UserComponent},
     {path:'login', component:LoginComponent},
     {path:'register', component:RegisterComponent}
 ];
